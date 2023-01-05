adadce92d10987049f9a88874ff5ad0e5f47894a
NaufalXDs <naufaldes07@gmail.com>
2023-01-05 16:23:56 +0700

    Fix Mobile Device

--- components/Hero.js
+++ components/Hero.js
@@ -11,11 +11,13 @@ export default function Hero() {
           <p className="py-4 text-center lg:text-start">
             I am{" "}
             <span className="text-red-500">
-              web developer <span className="text-slate-600">&</span> minecraft developer
+              Human
+              {/* web developer <hr className="invisible"></hr>
+              developer minecraft server */}
             </span>
           </p>
-          <p className="flex gap-1 text-red-400 text-center md:text-start">
-            <span>Study Deep: </span>
+          <p className="flex gap-1 text-red-400 text-center justify-center md:text-start">
+            <span>Study: </span>
             <a href="https://en.wikipedia.org/wiki/HTML">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="24" height="24" className="" fill="#f16524">
                 <path d="M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z" />
